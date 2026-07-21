f7fe9e68125d055b9b48a09701e76ea57ac99a70
SakkarinJija <k.sakkarin@pccm.ac.th>
2026-07-21 18:19:10 +0700

    Connect Google Sheets backend

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
-  API_URL: "", // วาง Google Apps Script Web App URL ที่ลงท้ายด้วย /exec
+  API_URL: "https://script.google.com/macros/s/AKfycbyPml7fcTckhR7riHtBcCGh8F7s63r6zxbkERrjxUvUJ5PrMLaSRzqZ9xVhlbbpV1beMQ/exec",
   CLASS_CODE: "ENGLISH6",
   APP_TITLE: "English 6-Level Learning Journey",
-  SCHOOL_NAME: "Your School"
+  SCHOOL_NAME: "PCSHS Mukdahan",
 };
